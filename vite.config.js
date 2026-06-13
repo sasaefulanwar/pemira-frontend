ba26b645610e9a53659190b493897ca4dc2b6ce6
Nur Saeful Anwar <saaefulanwar53094@gmail.com>
2026-06-14 04:12:10 +0700

    add vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  preview: {
+    allowedHosts: ["pemirarpl2026.online", "www.pemirarpl2026.online"],
+  },
+  server: {
+    allowedHosts: ["pemirarpl2026.online", "www.pemirarpl2026.online"],
+  },
 });
